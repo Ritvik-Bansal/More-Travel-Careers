686127bbf362a6dd1a5286f90850ba92fa09d546
Madhur Bansal <madhur97bansal@gmail.com>
2024-04-01 00:17:30 +0530

    fixes

--- src/JobOpenings.js
+++ src/JobOpenings.js
@@ -15,6 +15,8 @@ import {
   Link,
   Button,
 } from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendar, faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons';
 
 import './ApplicantsModal.css';
 
@@ -132,8 +134,11 @@ const JobOpenings = () => {
               <h3>{job.name}</h3>
               <h5>
                 <pre>
-                  <i className="fas fa-map-marker-alt"></i> {job.location}{' '}
-                  <i className="far fa-calendar"></i> {job.datePosted}
+                  <FontAwesomeIcon icon={faMapMarkedAlt} />
+                  <span>{` ${job.location}`}</span>
+                  <span>{`        `}</span>
+                  <FontAwesomeIcon icon={faCalendar} />
+                  <span>{` ${job.datePosted}`}</span>
                 </pre>
               </h5>
               <p>{job.description}</p>
@@ -184,131 +189,6 @@ const JobOpenings = () => {
               )}
             </li2>
           ))}
-          {/* <li2 data-location="Seattle">
-            <h3>Engineer</h3>
-            <h5>
-              <pre>
-                {' '}
-                <i className="fas fa-map-marker-alt"></i> Seattle, WA{' '}
-                <i className="far fa-calendar"></i> 11/27/2023
-              </pre>
-            </h5>
-            <p>
-              Passionate about travel? Join our team as a Travel Consultant,
-              where you'll assist clients in planning and booking memorable
-              trips, providing expert advice on destinations, accommodations,
-              and travel options.
-            </p>
-            <NavLink to="apply" className="cta-button">
-              Apply Now
-            </NavLink>
-          </li2>
-          <li2 data-location="Edmonton">
-            <h3>Customer Service Representative:</h3>
-            <h5>
-              <pre>
-                <i className="fas fa-map-marker-alt"></i> Edmonton, AB{' '}
-                <i className="far fa-calendar"></i> 11/11/2023
-              </pre>
-            </h5>
-            <p>
-              Elevate customer experiences as a Customer Service Representative.
-              Your role involves addressing inquiries, resolving issues, and
-              ensuring our clients receive exceptional support throughout their
-              travel journey.
-            </p>
-            <NavLink to="apply" className="cta-button">
-              Apply Now
-            </NavLink>
-          </li2>
-          <li2 data-location="Seattle">
-            <h3>Sales Representative:</h3>
-            <h5>
-              <pre>
-                <i className="fas fa-map-marker-alt"></i> Seattle, WA{' '}
-                <i className="far fa-calendar"></i> 09/04/2023
-              </pre>
-            </h5>
-            <p>
-              Join us as a Sales Representative and showcase the wonders of
-              travel to our clients. You'll be responsible for selling travel
-              packages, accommodations, and services, turning dreams into
-              unforgettable adventures.
-            </p>
-            <NavLink to="apply" className="cta-button">
-              Apply Now
-            </NavLink>
-          </li2>
-          <li2 data-location="New Delhi">
-            <h3>Travel Coordinator:</h3>
-            <h5>
-              <pre>
-                <i className="fas fa-map-marker-alt"></i> New Delhi, India{' '}
-                <i className="far fa-calendar"></i> 12/04/2023
-              </pre>
-            </h5>
-            <p>
-              Become a Travel Coordinator and orchestrate seamless travel
-              experiences. Manage itineraries, coordinate logistics, and ensure
-              our clients' journeys are well-planned and stress-free.
-            </p>
-            <NavLink to="apply" className="cta-button">
-              Apply Now
-            </NavLink>
-          </li2>
-          <li2 data-location="Seattle">
-            <h3>Digital Marketing Specialist:</h3>
-            <h5>
-              <pre>
-                <i className="fas fa-map-marker-alt"></i> Seattle, WA{' '}
-                <i className="far fa-calendar"></i> 10/28/2023
-              </pre>
-            </h5>
-            <p>
-              Shape the online presence of our travel agency as a Digital
-              Marketing Specialist. Develop and implement digital strategies to
-              promote our services, engage audiences, and inspire wanderlust.
-            </p>
-            <NavLink to="apply" className="cta-button">
-              Apply Now
-            </NavLink>
-          </li2>
-          <li2 data-location="Edmonton">
-            <h3>Technology and Systems Manager:</h3>
-            <h5>
-              <pre>
-                <i className="fas fa-map-marker-alt"></i> Edmonton, AB{' '}
-                <i className="far fa-calendar"></i> 09/27/2023
-              </pre>
-            </h5>
-            <p>
-              Join our tech-savvy team as a Technology and Systems Manager.
-              Oversee the smooth operation of our booking systems and website,
-              ensuring that our technological infrastructure enhances the travel
-              experience.
-            </p>
-            <NavLink to="apply" className="cta-button">
-              Apply Now
-            </NavLink>
-          </li2>
-          <li2 data-location="Edmonton">
-            <h3>Corporate Travel Consultant:</h3>
-            <h5>
-              <pre>
-                <i className="fas fa-map-marker-alt"></i> Edmonton, AB{' '}
-                <i className="far fa-calendar"></i> 10/18/2023
-              </pre>
-            </h5>
-            <p>
-              Navigate the world of business travel as a Corporate Travel
-              Consultant. Specialize in arranging business trips, managing
-              corporate accounts, and providing top-notch service to our
-              corporate clients.
-            </p>
-            <NavLink to="apply" className="cta-button">
-              Apply Now
-            </NavLink>
-          </li2> */}
         </ul2>
       </section>
       <Modal
